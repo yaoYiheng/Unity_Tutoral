de0acbee85dd4cd0a259f59ca509b5ef9416fd1b
yaoYiheng <fn1117@qq.com>
2018-08-08 16:55:52 +0800

    给一个物体添加移动, 发射脚本
    MainFunc:
            float turnMove = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
            float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
    
            transform.Translate(Vector3.forward * move);
            transform.Rotate(Vector3.up * turnMove);
    
    ============
    Fire
            Rigidbody shellIns = Instantiate(shell, firpos.position, shell.rotation) as Rigidbody;
    
            shellIns.AddForce(firpos.forward * speed);

---
+++ 02TipAndTrick/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    public float turnSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MovementDemo();
+	}
+
+    private void MovementDemo ()
+    {
+        float turnMove = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * move);
+        transform.Rotate(Vector3.up * turnMove);
+    }
+}
---
+++ 02TipAndTrick/Assets/Scripts/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public Transform firpos;
+    public Rigidbody shell;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        Rigidbody shellIns = Instantiate(shell, firpos.position, shell.rotation) as Rigidbody;
+
+        //Vector3 fire = firpos.forward * speed;
+        shellIns.AddForce(firpos.forward * speed);
+
+    }
+}
