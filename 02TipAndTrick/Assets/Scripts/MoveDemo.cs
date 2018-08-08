e51714c855153a3dfe3a1c09d22a1e4e579fade9
yaoYiheng <fn1117@qq.com>
2018-08-08 13:15:28 +0800

    平移与旋转
    transform.Translate()
    Moves the transform by x along the x axis, y along the y axis, and z along the z axis.
    
    移动变换由x沿着x轴，y沿着y轴，z沿着z轴。
    
    ===========
    transform.Rotate()
    Applies a rotation of zAngle degrees around the z axis, xAngle degrees around the x axis, and yAngle degrees around the y axis (in that order).
    
    应用一个旋转角度，zAngle度围绕z轴，xAngle度围绕x轴，yAngle度围绕y轴（这样的顺序）。

--- 02TipAndTrick/Assets/Scripts/MoveDemo.cs
+++ 02TipAndTrick/Assets/Scripts/MoveDemo.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class MoveDemo : MonoBehaviour {
 
+    public float speed;
+    public float turnSpeed;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+        }
+    }
 }
