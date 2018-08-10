1c70712bcc006e666105d25d0f4d9ebbe1efff14
yaoYiheng <fn1117@qq.com>
2018-08-11 01:09:09 +0800

    减少检视视图空间的小技巧
    当需要在一个对象上添加刚体时, 可以通过在脚本class上面添加[RequireComponent(typeof(Rigidbody))], 在该脚本被添加到游戏对象上时, 就会自动为该游戏对象添加一个Rigidbody的组件. 可以通过Reset()方法, rigidbody = GetComponent<Rigidbody>();为引用赋值.(没看明白这一步的含义是什么.
    
    可以在引用前添加[HideInInspector], 使其在检视视图中隐蔽.

---
+++ 02TipAndTrick/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour {
+
+
+    [HideInInspector]public Rigidbody rig;
+
+    private void Reset()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
