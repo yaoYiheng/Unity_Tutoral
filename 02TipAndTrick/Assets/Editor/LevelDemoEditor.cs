cce47d637cdbe8dc16f872c6f6833a4c7debbe01
yaoYiheng <fn1117@qq.com>
2018-08-10 17:44:21 +0800

    编写检视视图
    1. 任何检视视图的代码需写在Editor文件夹下.
    
    2. 需要用UnityEditor这个命名空间,
    需要在class前写上 [CustomEditor(typeof(编写检视视图的类名)]
    且该类继承自Editor.

---
+++ 02TipAndTrick/Assets/Editor/LevelDemoEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelDemo))]
+public class LevelDemoEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        //重写的话会调用父类的方法. 可以删除掉
+        //base.OnInspectorGUI();
+
+        LevelDemo myDemo = (LevelDemo)target;
+        //在检视视图中创建一个文本输入框
+        myDemo.experience = EditorGUILayout.IntField("Experience", myDemo.experience);
+        //添加一个label
+        //访问Level的get方法, 并将其转成字符串.
+        EditorGUILayout.LabelField("Level", myDemo.Level.ToString());
+    }
+
+}
---
+++ 02TipAndTrick/Assets/Scripts/LevelDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDemo : MonoBehaviour {
+
+	// Use this for initialization
+
+    public int experience;
+
+
+    public int Level
+    {
+        get { return experience / 500; }
+    }
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
