dddd64d07c9a6fcdc276373e1d09153fe11b1870
yaoYiheng <fn1117@qq.com>
2018-08-10 22:15:52 +0800

    在自定义的检视视图中添加一个按钮
    与往检视视图中添加之前的控件一样,
    这里新的知识点就是,
    if (GUILayout.Button("添加新物体"))
            {
                //调用脚本引用的对象的方法
                builderDemo.Build();
            }
    GUILayout.Button("添加新物体")创建一个可交互的按钮, 且返回值为bool类型, 在被点击之后, 执行指定的方法

---
+++ 02TipAndTrick/Assets/Editor/BuilderDemoEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectBuilderDemo))]
+public class BuilderDemoEditor : Editor {
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        //在该方法中, 有一个指向需要自定义的脚本的引用target
+        //通过该引用, 可以访问到那个脚本中的属性与方法
+        ObjectBuilderDemo builderDemo = (ObjectBuilderDemo)target;
+        if (GUILayout.Button("添加新物体"))
+        {
+            //调用脚本引用的对象的方法
+            builderDemo.Build();
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 02TipAndTrick/Assets/Scripts/ObjectBuilderDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBuilderDemo : MonoBehaviour {
+
+    public GameObject obj;
+    public Vector3 spawnPoint;
+
+    public void Build()
+    {
+        Instantiate(obj, spawnPoint, Quaternion.identity);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
