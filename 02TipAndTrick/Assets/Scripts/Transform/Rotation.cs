2ad4124e8910f1a6cf850e626bf0e821ac95478f
yaoYiheng <fn1117@qq.com>
2018-08-14 12:04:28 +0800

    .

--- 02TipAndTrick/Assets/Scripts/Transform/Rotation.cs
+++ 02TipAndTrick/Assets/Scripts/Transform/Rotation.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour 
+public class Rotation : MonoBehaviour
 {
-
-    private void Update()
+    
+    private void Start()
     {
-        print(transform.position);
-        print(transform.localPosition);
+        //屏幕空间以像素定义，
+        //屏幕左下为(0, 0)，右上是(pixelWidth, pixelHeight)，Z的位置是以世界单位衡量的到相机的距离。
+        print(Camera.main.WorldToScreenPoint(gameObject.transform.position));
+        print(gameObject.transform.localPosition);
+
     }
 }
