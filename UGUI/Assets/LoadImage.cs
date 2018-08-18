4f92145d589f8954e62b07d110f6fc39b90f14bd
yaoYiheng <fn1117@qq.com>
2018-08-18 16:01:07 +0800

    通过RawImage加载网络图片
    查看LoadImage.cs

---
+++ UGUI/Assets/LoadImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LoadImage : MonoBehaviour {
+
+
+    private string url = "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=785011176,3267677792&fm=173&app=25&f=JPEG?w=640&h=960&s=9180F117490EF4CE0A4590DE03008060";
+    public RawImage rawImage;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(loadBmp());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator loadBmp()
+    {
+        WWW wWW = new WWW(url);
+
+        yield return wWW;
+
+        rawImage.texture = wWW.texture;
+    }
+}
