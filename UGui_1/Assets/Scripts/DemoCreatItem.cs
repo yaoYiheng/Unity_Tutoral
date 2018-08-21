05724de94101666d1550b1a151e1146d323dbedb
yaoYiheng <fn1117@qq.com>
2018-08-21 13:10:07 +0800

    游戏商店页面
    知识点: 通过Grip Layout Group与ContentSizeFitter的组合实现对重复布局大小相同的item的效果.
    
    组件ScrollRect可以为页面增加滚动效果.
    
    UI组件中添加ScrollBar
    
     通过UIImage +Mask实现遮罩

---
+++ UGui_1/Assets/Scripts/DemoCreatItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCreatItem : MonoBehaviour {
+
+    public GameObject itemPrefab;
+
+    public int count;
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject item = Instantiate(itemPrefab);
+            //需要将该item的transform转换成RectTransform
+            RectTransform itemTransform = item.transform as RectTransform;
+            //设置到parent上
+            itemTransform.SetParent(transform);
+            item.transform.position = Vector3.zero;
+            item.transform.localScale = Vector3.one;
+            item.transform.rotation = Quaternion.Euler(Vector3.zero);    
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
