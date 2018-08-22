191874f7188bdd09e87d2663f17ca050228c41e6
yaoYiheng <fn1117@qq.com>
2018-08-22 16:47:52 +0800

    通过枚举来实现一个下拉菜单的显示
    在想要编辑的面板中定义一个枚举.

--- Plugin/Assets/Editor/CustomEditor/DemoCustomEditor.cs
+++ Plugin/Assets/Editor/CustomEditor/DemoCustomEditor.cs
@@ -7,24 +7,47 @@ using UnityEditor;
 public class DemoCustomEditor : Editor {
 
     //public SerializedObject mObj;
+    public SerializedProperty mEnum;
     public SerializedProperty a;
     public SerializedProperty b;
     public SerializedProperty myClass;
-
+    public SerializedProperty myColor;
     public void OnEnable()
     {
-        //mObj = new SerializedObject(target);
+        mEnum = serializedObject.FindProperty("myEnum");
         a = serializedObject.FindProperty("a");
         b  = serializedObject.FindProperty("b");
         myClass = serializedObject.FindProperty("myClass");
+        myColor = serializedObject.FindProperty("myColor");
     }
 
     public override void OnInspectorGUI()
     {
+        //更新
         serializedObject.Update();
-        EditorGUILayout.PropertyField(a);
-        EditorGUILayout.PropertyField(b);
-        EditorGUILayout.PropertyField(myClass, true);
+
+        //绘制出枚举
+        EditorGUILayout.PropertyField(mEnum);
+
+        //根据枚举的不同值, 来决定绘制不同的界面
+        switch(mEnum.enumValueIndex)
+        {
+            case 1:
+                EditorGUILayout.PropertyField(a);
+                break;
+            case 2:
+                EditorGUILayout.PropertyField(b);
+                break;
+            case 3:
+                EditorGUILayout.PropertyField(myClass, true);
+                break;
+            case 4:
+                EditorGUILayout.PropertyField(myColor);
+                break;
+        }
+
+
+        //应用
         serializedObject.ApplyModifiedProperties();
     }
 }
--- Plugin/Assets/Scripts/Test3.cs
+++ Plugin/Assets/Scripts/Test3.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 public class Test3 : MonoBehaviour {
-    
+    public MyEnum myEnum;
     public int a = 10;
     public float b = 10f;
-
     public MyClass myClass;
+    public Color myColor;
 	// Use this for initialization
 	void Start () {
 		
@@ -24,4 +24,12 @@ public class MyClass
 {
     public string name;
     public float heigh;
+}
+public enum MyEnum
+{
+    None,
+    IntVaule,
+    FloatValue,
+    ClassValue,
+    ColorValue
 }
