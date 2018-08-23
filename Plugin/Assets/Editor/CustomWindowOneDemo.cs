480d24f8420e35137b62dadc09718b6dd07b5934
yaoYiheng <fn1117@qq.com>
2018-08-23 17:36:00 +0800

    单选/ 折叠 /包裹

--- Plugin/Assets/Editor/CustomWindowOneDemo.cs
+++ Plugin/Assets/Editor/CustomWindowOneDemo.cs
@@ -15,6 +15,8 @@ public class CustomWindowOneDemo : EditorWindow
     private int myLayer;
     private Object myGO;
 
+
+    private bool isSelected;
     [MenuItem("Menu/窗口一")]
     public static void ShowWindow()
     {
@@ -42,17 +44,35 @@ public class CustomWindowOneDemo : EditorWindow
         index = EditorGUILayout.IntPopup(index, strs, ints);
 
         EditorGUILayout.Space();
-        EditorGUILayout.LabelField("标签");
-        myTag = EditorGUILayout.TagField(myTag);
-        EditorGUILayout.Space();
 
-        EditorGUILayout.LabelField("层级");
-        myLayer = EditorGUILayout.LayerField(myLayer);
-        EditorGUILayout.Space();
+        //单字选择
+        isSelected = EditorGUILayout.Toggle("是否折叠", isSelected);
 
-        EditorGUILayout.LabelField("对象选择");
-        myGO = EditorGUILayout.ObjectField(myGO, typeof(Transform));
-        EditorGUILayout.Space();
+        if(isSelected = EditorGUILayout.Foldout(isSelected, "折叠"))
+        {
+            //在折叠的外围包裹一层, 需要与结尾的代码相互配合
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUI.indentLevel++;
+
+            //标签选择器
+            EditorGUILayout.LabelField("标签");
+            myTag = EditorGUILayout.TagField(myTag);
+            EditorGUILayout.Space();
+
+            //层级选择器
+            EditorGUILayout.LabelField("层级");
+            myLayer = EditorGUILayout.LayerField(myLayer);
+            EditorGUILayout.Space();
+
+            //对象选择器
+            EditorGUILayout.LabelField("对象选择");
+            myGO = EditorGUILayout.ObjectField(myGO, typeof(Transform));
+            EditorGUILayout.Space();
+
+            EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+        }
+
 
     }
 }
