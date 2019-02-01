fbc9a2a80c8a3692c233f320b6e0e202f948a625
yaoYiheng <fn1117@qq.com>
2019-02-01 16:53:02 +0800

    网络通信

---
+++ UnityPESocket/Assets/ClientSession.cs
@@ -0,0 +1,29 @@
+﻿/****************************************************
+    文件：ClientSession.cs
+	作者：Yiheng
+    邮箱: fn1117@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PENet;
+using Protocal;
+public class ClientSession : PENet.PESession<NetMessage>
+{
+    protected override void OnConnected()
+    {
+        Debug.Log("Server Connet");
+    }
+
+    protected override void OnDisConnected()
+    {
+        Debug.Log("Client DisConnet");
+    }
+
+    protected override void OnReciveMsg(NetMessage msg)
+    {
+         Debug.Log("Server Rsp:" + msg.text);
+    }
+}
---
+++ UnityPESocket/Assets/GameStart.cs
@@ -0,0 +1,61 @@
+﻿/****************************************************
+    文件：GameStart.cs
+	作者：Yiheng
+    邮箱: fn1117@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Protocal;
+using PENet;
+public class GameStart : MonoBehaviour 
+{
+
+    public PESocket<ClientSession, NetMessage> client;
+    
+    private void Start()
+    {
+        //在客户端新建一个Socket
+        client = new PESocket<ClientSession, NetMessage>();
+        //配置Ip和端口
+        client.StartAsClient(IPConfigure.srvIP, IPConfigure.srePort);
+        //设置打印日志的接口
+        client.SetLog(true, (string message, int level) =>
+        {
+            message = "Log:" + message;
+            switch (level)
+            {
+                case 0:
+
+                    Debug.Log(message);
+                    break;
+
+                case 1:
+
+                    Debug.LogWarning(message);
+                    break;
+
+                case 2:
+                    Debug.LogError(message);
+                    break;
+
+                case 3:
+                    Debug.Log(message);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            client.session.SendMsg(new NetMessage() { text = "Hello Unity" });
+        }
+    }
+}
